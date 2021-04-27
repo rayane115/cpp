bdcb60fb5aa5550613765ea099189fabc0297c9a
user42 <rayane.qouchich@gmail.com>
2021-04-27 17:58:16 +0200

    ex01

--- cp06/ex01/main.cpp
+++ cp06/ex01/main.cpp
@@ -1,16 +1,51 @@
-#include "main.hpp"
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+struct Data { std::string s1; int n; std::string s2; };
 
 void * serialize(void)
 {
+    /*fonction qui vas cree deux string avec des lettre aleatoire
+    et un nombre aleatoire */
+    srand((unsigned int)time(0));
+    Data *raw = new Data;
+
+    std::string	alphanum("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+    for (int i = 0; i < 8 ; i++)
+    {
+        /* 8 tour de boucle en donnant une lettre aleatoire pour s1 et s2 */
+        raw->s1 += alphanum[rand() % alphanum.length()];
+		raw->s2 += alphanum[rand() % alphanum.length()];
+    }
+    raw->n = rand();
+    std::cout << "/********************serialize***********************/ " << std::endl;
+    std::cout << "s1 = " << raw->s1 << std::endl;
+    std::cout << "nbr = " << raw->n << std::endl;
+    std::cout << "s2 = " << raw->s2 << std::endl;
+    return raw;
 
 }
 
 Data * deserialize(void * raw)
 {
-    
+    return (reinterpret_cast<Data*>(raw));
 }
 
 int main()
 {
+    Data *data_s;
+    Data *data_d;
+    /*obliger de re interpreter car la fonction renvoi un pointeur de type void*/
+    data_s = (reinterpret_cast<Data*>(serialize()));
+    data_d = deserialize(data_s);
+    std::cout << "/********************deserialize***********************/ " << std::endl;
+    std::cout << "s1 = " << data_d->s1 << std::endl;
+    std::cout << "nbr = " << data_d->n << std::endl;
+    std::cout << "s2 = " << data_d->s2 << std::endl;
+    delete data_s;
+    delete data_d;
+
     return 0;
 }
--- cp06/ex01/main.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_HPP
-# define MAIN_HPP
-
-#include <iostream>
-
-void * serialize(void);
-Data * deserialize(void * raw);
-struct Data { std::string s1; int n; std::string s2; };
-#endif
